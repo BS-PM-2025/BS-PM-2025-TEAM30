e4f5eb128ab5a292be4806cd7db4539b5fee4d24
adi stamker <adistamker88@gmail.com>
2025-03-31 16:05:35 +0300

    עדכון של חיבור POSTGRESQL

--- backend/settings.py
+++ backend/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres@team30.postgres.database.azure.com',
+        'USER': 'team30',
+        'PASSWORD': 'insasce30!',
+        'HOST': 'team30.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
