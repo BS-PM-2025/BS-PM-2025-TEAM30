60bf0717fb0cd07cba8d8ee5db7e824a472d829e
snirpi <sni124r@gmail.com>
2025-04-18 19:39:24 +0300

    דיווח תקלה יש בעיה ביצירת טבלה שתשמור את כל הדיווחים

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from backend.Report.views import ReportIssueView
 
 urlpatterns = [
     path('api/', include('backend.user.urls')),
     path('admin/', admin.site.urls),
+    path('report/', ReportIssueView.as_view(), name='report'),
 ]
