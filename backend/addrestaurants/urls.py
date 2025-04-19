7b3b0f17c22176a7bd690b31354fecd90786d934
snirpi <sni124r@gmail.com>
2025-04-19 18:29:28 +0300

    הוספת מסעדה מאיזור אדמין

--- backend/addrestaurants/urls.py
+++ backend/addrestaurants/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from .views import detect_restaurant
 
 urlpatterns = [
-    path('detect/', detect_restaurant, name='detect-restaurant'),
 ]
--- backend/settings.py
+++ backend/settings.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # ⬅ להוסיף ראשון
+    'django.middleware.common.CommonMiddleware',  # ⬅ אפשר להוסיף אחרי זה
 ]
 
 
