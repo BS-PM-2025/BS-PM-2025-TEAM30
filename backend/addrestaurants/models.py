5e9fe41daaecac48f24a2859fddc0dd49f19709d
snirpi <sni124r@gmail.com>
2025-04-19 22:22:47 +0300

    הוספת מסעדה יש בעיה לא נוסף לדאטה בייס

--- backend/addrestaurants/models.py
+++ backend/addrestaurants/models.py
@@ -7,13 +7,9 @@ class Restaurant(models.Model):
     phone = models.CharField(max_length=20)
     is_open = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    location = models.CharField(max_length=200)
-    website = models.URLField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    is_promoted = models.BooleanField(default=False)
-
-    latitude = models.FloatField(null=True, blank=True)
-    longitude = models.FloatField(null=True, blank=True)
+    location = models.CharField(max_length=200)  # עיר/מיקום
+    website = models.URLField(blank=True, null=True)  # כתובת אתר (אופציונלי)
+    description = models.TextField(blank=True, null=True)  # תיאור כללי (אופציונלי)
 
     def __str__(self):
         return self.name
