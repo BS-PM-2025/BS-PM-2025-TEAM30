383b129aa3b345b299276e47781a1b1bda55b0a6
Idantegave7 <158479599+Idantegave7@users.noreply.github.com>
2025-04-19 13:08:48 +0300

    עדכון שכחתי סיסמא ואיפוס סופי

--- backend/settings.py
+++ backend/settings.py
@@ -138,5 +138,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'idantegave777@gmail.com'  # כאן תכניס את כתובת הג'ימייל שלך
+EMAIL_HOST_PASSWORD = 'yeyj pcjd kpok minx'  # הסיסמה שקיבלת מהשלב הקודם
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 CORS_ALLOW_ALL_ORIGINS = True
