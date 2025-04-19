f5d1f9de5c408f128a358a04fabad421536311c2
snirpi <sni124r@gmail.com>
2025-04-19 22:22:47 +0300

    הוספת מסעדה יש בעיה לא נוסף לדאטה בייס

--- frontend-clean/src/App.js
+++ frontend-clean/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
+import AddRestaurantPage from './pages/AddRestaurantPage';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/register" />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/addrestaurant" element={<AddRestaurantPage />} />
       </Routes>
     </Router>
   );
