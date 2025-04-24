a9345fb3d227d8b1320cb83fdc906e5700a6ce98
snirpi <sni124r@gmail.com>
2025-04-24 20:05:16 +0300

    git commit -m "BSPM25T30-32 <message>"
    אין תקלה מוסיף אתר

--- backend/addrestaurants/models.py
+++ backend/addrestaurants/models.py
@@ -7,9 +7,10 @@ class Restaurant(models.Model):
     phone = models.CharField(max_length=20)
     is_open = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    location = models.CharField(max_length=200)  # עיר/מיקום
-    website = models.URLField(blank=True, null=True)  # כתובת אתר (אופציונלי)
-    description = models.TextField(blank=True, null=True)  # תיאור כללי (אופציונלי)
+    location = models.CharField(max_length=200)
+    website = models.URLField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    is_promoted = models.BooleanField(default=False)  # האם המסעדה מקודמת?
 
     def __str__(self):
         return self.name
