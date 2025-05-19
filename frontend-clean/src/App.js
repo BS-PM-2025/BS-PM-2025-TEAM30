203efc518ca96e82ee36251a5a6190d090b0daf9
adi Stamker <adistamker88@gmail.com>
2025-05-20 00:47:19 +0300

    git commit -m "BSPM25T30-12 <הוספה של איתור מיקום שלי והצגת מסעדות בעיר בה אני נמצא >"
    git commit -m "BSPM25T30-2 <בנייה התאמה ואופציה לשמירה של דברים רק עבור משתמש מחובר ורשום כל שאר הפעולות לא ניתנות לביצוע >"

--- frontend-clean/src/App.js
+++ frontend-clean/src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<MapComponent />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:uid/:token" element={<ResetPassword />} />
