dad6aaafb2d65d25f05e39ceb2273d81e78cd8db
Nitaybenadmon123 <nitaybe@ac.sce.ac.il>
2025-05-20 15:43:45 +0300

    תיקון

--- backend/restaurants/urls.py
+++ backend/restaurants/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from backend.restaurants.views import get_popular_times_view
+from django.urls import path, include
 
 
 urlpatterns = [
     path('api/save-restaurant/', views.save_restaurant),
     path('api/get-saved/', views.get_saved_restaurants),
     path('api/load/', get_popular_times_view),
+    path('api/', include('backend.restaurants.urls')),
 ]
